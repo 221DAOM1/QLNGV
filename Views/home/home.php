d238878c8728937c3199a14cb47a2a152aa21c3a
huykt1111 <huylmht10@gmail.com>
2022-05-25 08:53:51 +0700

    conten-home

--- Views/home/home.php
+++ Views/home/home.php
@@ -13,4 +13,82 @@
         </ul>
     </div>
 </section>
-<script src="./Publics/js/countdown.js"></script>
+<section id="content_home" style="position:relative">
+    <div class="container"> 
+        <div class="content_home_top">
+            <div class="big-hotline  wow fadeInDown animated">
+                <i class="fa fa-phone-square"></i>
+                0901726898 -  0981188089
+            </div>
+            <div class="big-hotline slogan-left wow fadeInDown animated">
+                    Tận tâm giúp đỡ - Tận tâm phục vụ
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-sm-12 wow fadeInDown animated" style="visibility: visible; animation-name: fadeInDown; -webkit-animation-name: fadeInDown;">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 mg-btm-1">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Giúp việc gia đình</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Chăm sóc trẻ em</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                            
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Giúp việc giờ theo hành chính</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Giúp việc tết nguyên đán</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Giúp việc mùa hè</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                            <div class="col-md-6 col-sm-6 col-xs-12">
+                                <p class="link-btn big-a">
+                                <a class="btn btn-success" style="width:100%" href="">
+                                    <span> Giúp việc chăm sóc người già</span>
+                                        <i class="fa fa-chevron-circle-right"></i>
+                                </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 wow fadeInDown animated" style="visibility: visible; animation-name: fadeInDown; -webkit-animation-name: fadeInDown;">
+                <div class="well bg-yellow">
+                    <h5>PHƯƠNG CHÂM CỦA CHÚNG TÔI</h5>
+                        <p><span style="color:#ffffe0;">Gíúp người lao động có việc làm và lo chỗ ăn ở<u><strong> hoàn toàn miễn phí .&nbsp;</strong></u></span>&nbsp;&nbsp;<span style="color:#006400;"><strong>TẬN TÂM GIÚP ĐỠ</strong></span></p>
+                        <p><span style="color:#ffffe0;"><u><strong>Được </strong>đổi <strong>người miễn phí </strong></u>,cam kết uy tín tuyệt đối với nhà tuyển dụng .</span><span style="color:#008000;"><strong>TÂN TÂM PHỤC VỤ</strong></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
