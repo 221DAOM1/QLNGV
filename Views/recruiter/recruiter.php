c6e8b39c3f9b8f543fe6a34f68f4c9fb515961db
VoXuanPhuc <phucpro746@gmail.com>
2022-06-15 08:35:21 +0700

    validate form post

--- Views/recruiter/recruiter.php
+++ Views/recruiter/recruiter.php
@@ -1,40 +1,44 @@
 <section id="main">
     <div class="container">
         <div class="page-content-recruiter">
-            <h1>Đăng thông tin công việc</h1>
+            <div>
+                <h1 style="text-align: center ;">Đăng thông tin công việc</h1>
+            </div>
             <hr>
             <?php if (isset($_COOKIE['msg2'])) { ?>
                 <div class="alert alert-success">
                     <strong>Thông báo</strong> <?= $_COOKIE['msg2'] ?>
                 </div>
             <?php } ?>
-            <form id ="table" action="?act=work&xuli=store" method="POST" enctype="multipart/form-data">
+            <form id="table" action="?act=work&xuli=store" method="POST" enctype="multipart/form-data">
                 <fieldset class="form-group">
                     <label>Tên công việc</label>
                     <span class="required">*</span>
-                    <input type="text" name="tenCV" class="form-control form-control-lg" placeholder="Tên công việc"/>
+                    <input type="text" name="tenCV" class="form-control form-control-lg" placeholder="Tên công việc" required />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Loại công việc</label>
-                    <select id="maDMCV" class="form-control" name="maDMCV">
+                    <select required id="maDMCV" class="form-control" name="maDMCV">
+                        <option value="" disabled selected>Chọn loại công việc</option>
                         <?php foreach ($data_category as $row) { ?>
-                        <option value="<?= $row['maDMCV'] ?>"><?= $row['tenDM'] ?></option>
-                        <?php } ?> 
+                            <option value="<?= $row['maDMCV'] ?>"><?= $row['tenDM'] ?></option>
+                        <?php } ?>
                     </select>
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Khu vực</label>
-                    <select id="khuVuc" class="form-control" name="khuVuc">
+                    <select required id="khuVuc" class="form-control" name="khuVuc">
+                        <option value="" disabled selected>Chọn khu vực</option>
                         <?php foreach ($data_district as $row) { ?>
-                        <option value="<?= $row['idQuan'] ?>"><?= $row['tenQuan'] ?></option>
-                        <?php } ?> 
+                            <option value="<?= $row['idQuan'] ?>"><?= $row['tenQuan'] ?></option>
+                        <?php } ?>
                     </select>
                 </fieldset>
                 <p class="recruiter_check_text">Điều kiện làm việc</p>
                 <hr>
                 <fieldset class="form-group">
                     <label>Số lượng người làm</label>
-                    <input type="text" name="soLuong" class="form-control form-control-lg" placeholder="Số lượng người làm"/>
+                    <input required type="text" name="soLuong" class="form-control form-control-lg" placeholder="Số lượng người làm" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Trình độ học vấn</label>
@@ -46,7 +50,7 @@
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Thời gian làm việc</label>
-                    <input type="text" name="time" class="form-control form-control-lg" placeholder="Thời gian làm việc"/>
+                    <input required type="text" name="time" class="form-control form-control-lg" placeholder="Thời gian làm việc" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Giới tính yêu cầu</label>
@@ -58,19 +62,19 @@
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Chức vụ người làm</label>
-                    <input type="text" name="chucVu" class="form-control form-control-lg" placeholder="Chức vụ người làm"/>
+                    <input type="text" name="chucVu" class="form-control form-control-lg" placeholder="Chức vụ người làm" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Lương nhân viên</label>
-                    <input type="text" name="luong" class="form-control form-control-lg" placeholder="Lương nhân viên"/>
+                    <input required type="text" name="luong" class="form-control form-control-lg" placeholder="Lương nhân viên" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Địa chỉ làm việc</label>
-                    <input type="text" name="diaChi" class="form-control form-control-lg" placeholder="Địa chỉ làm việc"/>
+                    <input required type="text" name="diaChi" class="form-control form-control-lg" placeholder="Địa chỉ làm việc" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Mô tả</label>
-                    <input type="text" name="moTa" class="form-control form-control-lg" placeholder="Mô tả"/>
+                    <input type="text" name="moTa" class="form-control form-control-lg" placeholder="Mô tả" />
                 </fieldset>
                 <fieldset class="form-group">
                     <label>Hình ảnh chính</label>
@@ -87,7 +91,7 @@
                 <div class="btn-recruiter">
                     <button class="btn-confirm" type="submit" form="table">Xác nhận</button>
                 </div>
-        </form>
+            </form>
         </div>
     </div>
 </section>
